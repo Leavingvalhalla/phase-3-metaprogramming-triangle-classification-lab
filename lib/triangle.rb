9bd1942b9447f84759822af2903474b6a091f46b
Zach Lefler <leaving.valhalla@gmail.com>
2022-04-09 15:10:14 -0700

    commit tree 5ef0e4e27f8f112e98898473d98cfffe864103d7

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,30 @@
 class Triangle
-  # write code here
+  
+attr_accessor :sides
+  def initialize(side1, side2, side3)
+    @sides = [side1, side2, side3].sort
+  end
+
+  def kind
+    self.sides.each do |side| 
+      if side <= 0
+        raise TriangleError
+      elsif self.sides[0] + self.sides[1] <= self.sides[2]
+        raise TriangleError
+      end
+    end
+    if @sides[0] == sides[1] && @sides[1] == sides[2]
+      :equilateral
+    elsif @sides[0] == @sides[1] || @sides[1] == @sides[2]
+      :isosceles
+    else
+      :scalene
+    end
+
+  end
+
+  class TriangleError < StandardError
+  end
+
+
 end
